dca2e738ed380f22e198c30f3ad0ceb9a4ce0177
Sam Hawkins <hawki2sl@dukes.jmu.edu>
2024-10-19 14:30:45 -0400

    Icons, navbar, etc

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -1,21 +1,60 @@
 import { Link } from "react-router-dom";
+import classes from "../Styles/NavBar.module.css";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-const Navbar: React.FC = () => {
+let id = 0;
+
+const incID = () => {
+  return ++id;
+};
+
+const navListItems = [
+  {
+    id: incID(),
+    path: "/",
+    label: "Home",
+  },
+  {
+    id: incID(),
+    path: "/upload",
+    label: "File Upload Page",
+  },
+  {
+    id: incID(),
+    path: "/insights",
+    label: "Insights Display Page",
+  },
+  {
+    id: incID(),
+    path: "#",
+    label: "Sign Up/Sign In",
+  },
+];
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/upload">File Upload Page</Link>
-        </li>
-        <li>
-          <Link to="/insights">Insights Display Page</Link>
-        </li>
-        <li>
-          <Link to="#">Sign Up/Sign In</Link>
-        </li>
+      <nav className={classes.nav_container}>
+      <FontAwesomeIcon
+        icon={faBars}
+        className={classes.hamburger}
+        onClick={toggleMenu}
+      />
+      <ul className={`${isOpen ? classes.nav_list_isOpen : classes.nav_list}`}>
+        {navListItems.map((item) => {
+          return (
+            <li key={item.id} className={classes.nav_item}>
+              <Link to={item.path}>{item.label}</Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
--- src/Routes/RootLayout.tsx
+++ src/Routes/RootLayout.tsx
@@ -1,24 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/NavBar";
+import classes from "../Styles/RootLayout.module.css";
 
 const RootLayout = () => {
   return (
-    <div>
-      <div>
-        <header>
-          <h1>My App Header</h1>
-          <nav>
-            <Navbar />
-          </nav>
-        </header>
-      </div>
+    <>
+      <header className={classes.header}>
+        <h1>My App Header</h1>
+        <Navbar />
+      </header>
       <main>
         <Outlet /> {/* This renders the child routes, such as App */}
       </main>
       <footer>
         <p>My Footer</p>
       </footer>
-    </div>
+    </>
   );
 };
 
